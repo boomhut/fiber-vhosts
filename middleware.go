ede91a7237130c412b62ddbd517363de8cf56709
boomhut <56619040+boomhut@users.noreply.github.com>
2024-01-23 03:22:34 +0100

    Update middleware.go

--- middleware.go
+++ middleware.go
@@ -1,6 +1,9 @@
 package vhosts
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+)
 
 // FiberHandler is the handler for the vhost middleware
 type FiberHandler func(*fiber.Ctx) error
@@ -10,7 +13,8 @@ type FiberErrorHandler func(*fiber.Ctx, error) error
 
 // VhostsHandler is the handler for the vhosts middleware
 func VhostsHandler(c *fiber.Ctx) error {
-	// Get the vhosts from the context
+	// debug("vhosts middleware")
+	log.Debugf("vhosts middleware %s", c.Hostname())
 
 	// Get the hostname from the request
 	hostname := c.Hostname()
@@ -18,9 +22,17 @@ func VhostsHandler(c *fiber.Ctx) error {
 	// Get the vhost with the given hostname
 	vhost, ok := vhosts.Get(hostname)
 	if !ok {
+		log.Debugf("vhost not found for hostname %s", hostname)
 		// Return a 404 if the vhost doesn't exist
 		return c.SendStatus(404)
 	}
+
+	log.Debugf("vhost found for hostname %s", hostname)
+	log.Debugf("vhost websiteID %s", vhost.WebsiteID)
+	log.Debugf("vhost path %s", vhost.Path)
+	log.Debugf("vhost lastModified %d", vhost.LastModified)
+	log.Debugf("vhost errorHandler %v", vhost.ErrorHandler)
+	log.Debugf("vhost handler %v", vhost.Handler)
 
 	// Set some values on the context
 	c.Locals("vhost.hostname", hostname)               // Hostname
